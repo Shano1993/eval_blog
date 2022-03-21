6d1f5dc3c355dc3929215b463b39b2c9d8ebbd79
Shano1993 <stefan.hanotiau@gmail.com>
2022-03-21 15:39:14 +0100

    blog

--- Config.php
+++ Config.php
@@ -5,7 +5,7 @@ namespace App;
 class Config
 {
     public const DB_CHARSET = 'utf8';
-    public const DB_NAME = 'eval_blog';
+    public const DB_NAME = 'blog_eval';
     public const DB_HOST = 'localhost';
     public const DB_USERNAME = 'root';
     public const DB_PASSWORD = '';
